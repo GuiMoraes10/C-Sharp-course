929026ef94267541622eb0de1c7288cc66287467
Guilherme de Oliveira Moraes <moraes@alfainstrumentos.com.br>
2024-09-09 14:46:10 -0300

    GetHashCode e Equals

---
+++ 53 GetHashCode e Equals/AP01/AP01/Entities/Client.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace AP01.Entities
+{
+    internal class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+
+        // sobrescrevendo o equals para personalizar a comparacao
+        public override bool Equals(object? obj)
+        {
+            if (!(obj is Client))
+            {
+                return false;
+            }
+
+            // digo que o obj recebido eh um Client
+            Client other = obj as Client;
+
+            // retorno o tipo de comparacao personalizada que eu quero fazer
+            return Email.Equals(other.Email);
+        }
+
+        // sobrescrevendo para implementacao do GetHashCode personalizado
+        public override int GetHashCode()
+        {
+            // apenas retorna o GetHashCode do valor desejado
+            return Email.GetHashCode();
+        }
+    }
+}
---
+++ 53 GetHashCode e Equals/AP01/AP01/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using AP01.Entities;
+
+namespace AP01
+{
+    class Program
+    {
+        // GetHashCode e Equals sao utilizadas para comparar se um objeto e igual a outro
+        // Equals: lento, mas resposta eh 100%
+        // GetHashCode; rapido, mas resposta nao eh 100%
+        // Tipos predefinidos como char, string e int ja possuem implementacoes para estas operacoes
+        // Classes e structs personalizadas precisam sobrepo-las
+        static void Main(string[] args)
+        {
+
+
+            // Equals
+
+            string a = "Maria";
+            string b = "Alex";
+            Console.WriteLine(a.Equals(b));
+
+
+            // GetHashCode
+
+            // se o codigo de dois objetos for diferentes, entao os objetos sao diferentes
+            // se o codigo de dois objetos for igual, provavelmente sao iguais (pode haver colisao)
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+
+            // para testar um por um, eh recomendado se utilizar o getHashCode, e ai sim utiliza o equals para confirmar, pois ele eh mais rapido
+
+
+            // EXEMPLO COM O OBJETO DE CLIENT
+
+            Client ca = new Client() { Name = "Maria", Email = "maria@gmail.com" };
+            Client cb = new Client() { Name = "Alex", Email = "alex@gmail.com" };
+
+            Console.WriteLine(ca.Equals(cb));
+            Console.WriteLine(ca.GetHashCode());
+            Console.WriteLine(cb.GetHashCode());
+        }
+    }
+}
