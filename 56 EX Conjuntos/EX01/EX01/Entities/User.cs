3ef51e7a7f8c23b6c35747cc37ea7ca84da35769
Guilherme de Oliveira Moraes <moraes@alfainstrumentos.com.br>
2024-09-11 15:12:49 -0300

    Ex conjuntos 1

---
+++ 56 EX Conjuntos/EX01/EX01/Entities/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EX01.Entities
+{
+    internal class User
+    {
+        string Name {  get; set; }
+        DateTime Date { get; set; }
+
+        public User(string name, DateTime date)
+        {
+            Name = name;
+            Date = date;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if(!(obj is User))
+            {
+                return false;
+            }
+
+            User other = obj as User;
+
+            return Name.Equals(other.Name);
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+    }
+}
---
+++ 56 EX Conjuntos/EX01/EX01/Program.cs
@@ -0,0 +1,43 @@
+﻿using EX01.Entities;
+using System;
+using System.Globalization;
+using System.IO;
+using System.Reflection.Metadata;
+
+namespace EX01
+{
+    class Program
+    {
+        // Um site de internet registra um log de acessos dos usuarios.
+        // Um registro de log consiste no nome de usuario e o instante em que
+        // o usuario acessou o site no padrao ISO 8601,separados por espaco, conforme exemplo.
+        // Fazer um programa que leia o log de acessos a partir de um arquivo,
+        // e dai informe quantos usuarios distintos acessam o site        
+
+        static void Main(string[] args)
+        {
+            string path = @"C:\temp\in.txt";
+            HashSet<User> users = new HashSet<User>();
+
+            try
+            {
+                using(StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        string[] lines = line.Split(" ");
+                        string Name = lines[0];
+                        DateTime Date = DateTime.Parse(lines[1]);
+                        users.Add(new User(Name, Date));                        
+                    }
+                    Console.WriteLine("Total users: " + users.Count);
+
+                }
+            }catch(IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
